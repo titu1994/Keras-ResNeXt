89902695e86eb1576444c8c6732a33b3be0b72bc
Somshubra Majumdar <titu1994@gmail.com>
2017-06-09 11:43:10 -0500

    Add images and update model for ImageNet support

--- resnext.py
+++ resnext.py
@@ -9,9 +9,9 @@ from __future__ import division
 import warnings
 
 from keras.models import Model
-from keras.layers.core import Dense, Activation, Flatten
+from keras.layers.core import Dense, Activation
 from keras.layers.convolutional import Conv2D
-from keras.layers.pooling import GlobalAveragePooling2D
+from keras.layers.pooling import GlobalAveragePooling2D, MaxPooling2D
 from keras.layers import Input
 from keras.layers.merge import concatenate, add
 from keras.layers.normalization import BatchNormalization
@@ -31,7 +31,7 @@ TF_WEIGHTS_PATH_NO_TOP = ''
 def ResNeXt(input_shape=None, depth=29, cardinality=8, width=4, weight_decay=5e-4,
             include_top=True, weights=None, input_tensor=None,
             classes=10):
-    """Instantiate the ResNeXt architecture Note that ,
+    """Instantiate the ResNeXt architecture. Note that ,
         when using TensorFlow for best performance you should set
         `image_data_format="channels_last"` in your Keras config
         at ~/.keras/keras.json.
@@ -40,7 +40,8 @@ def ResNeXt(input_shape=None, depth=29, cardinality=8, width=4, weight_decay=5e-
         convention used by the model is the one
         specified in your Keras config file.
         # Arguments
-            depth: number or layers in the ResNeXt model
+            depth: number or layers in the ResNeXt model. Can be an
+                integer or a list of integers.
             cardinality: the size of the set of transformations
             width: multiplier to the ResNeXt width (number of filters)
             weight_decay: weight decay (l2 norm)
@@ -148,14 +149,149 @@ def ResNeXt(input_shape=None, depth=29, cardinality=8, width=4, weight_decay=5e-
     return model
 
 
-def __initial_conv_block(input, weight_decay):
-    x = Conv2D(64, (3, 3), padding='same', use_bias=False, kernel_initializer='he_normal',
-               kernel_regularizer=l2(weight_decay))(input)
+def ResNeXtImageNet(input_shape=None, depth=[3, 4, 6, 3], cardinality=32, width=8, weight_decay=5e-4,
+                    include_top=True, weights=None, input_tensor=None,
+                    classes=1001):
+    """ Instantiate the ResNeXt architecture for the ImageNet dataset. Note that ,
+        when using TensorFlow for best performance you should set
+        `image_data_format="channels_last"` in your Keras config
+        at ~/.keras/keras.json.
+        The model are compatible with both
+        TensorFlow and Theano. The dimension ordering
+        convention used by the model is the one
+        specified in your Keras config file.
+        # Arguments
+            depth: number or layers in the ResNeXt model. Can be an
+                integer or a list of integers.
+            cardinality: the size of the set of transformations
+            width: multiplier to the ResNeXt width (number of filters)
+            weight_decay: weight decay (l2 norm)
+            include_top: whether to include the fully-connected
+                layer at the top of the network.
+            weights: `None` (random initialization) or `imagenet` (trained
+                on ImageNet)
+            input_tensor: optional Keras tensor (i.e. output of `layers.Input()`)
+                to use as image input for the model.
+            input_shape: optional shape tuple, only to be specified
+                if `include_top` is False (otherwise the input shape
+                has to be `(32, 32, 3)` (with `tf` dim ordering)
+                or `(3, 32, 32)` (with `th` dim ordering).
+                It should have exactly 3 inputs channels,
+                and width and height should be no smaller than 8.
+                E.g. `(200, 200, 3)` would be one valid value.
+            classes: optional number of classes to classify images
+                into, only to be specified if `include_top` is True, and
+                if no `weights` argument is specified.
+        # Returns
+            A Keras model instance.
+        """
 
+    if weights not in {'imagenet', None}:
+        raise ValueError('The `weights` argument should be either '
+                         '`None` (random initialization) or `imagenet` '
+                         '(pre-training on ImageNet).')
+
+    if weights == 'cifar10' and include_top and classes != 10:
+        raise ValueError('If using `weights` as CIFAR 10 with `include_top`'
+                         ' as true, `classes` should be 10')
+
+    if type(depth) == int:
+        if (depth - 2) % 9 != 0:
+            raise ValueError('Depth of the network must be such that (depth - 2)'
+                             'should be divisible by 9.')
+
+    # Determine proper input shape
+    input_shape = _obtain_input_shape(input_shape,
+                                      default_size=32,
+                                      min_size=8,
+                                      data_format=K.image_data_format(),
+                                      include_top=include_top)
+
+    if input_tensor is None:
+        img_input = Input(shape=input_shape)
+    else:
+        if not K.is_keras_tensor(input_tensor):
+            img_input = Input(tensor=input_tensor, shape=input_shape)
+        else:
+            img_input = input_tensor
+
+    x = __create_res_next_imagenet(classes, img_input, include_top, depth, cardinality, width, weight_decay)
+
+    # Ensure that the model takes into account
+    # any potential predecessors of `input_tensor`.
+    if input_tensor is not None:
+        inputs = get_source_inputs(input_tensor)
+    else:
+        inputs = img_input
+    # Create model.
+    model = Model(inputs, x, name='wide-resnet')
+
+    # load weights
+    if weights == 'cifar10':
+        if (depth == 29) and (cardinality == 8) and (width == 4):
+            # Default parameters match. Weights for this model exist:
+
+            if K.image_data_format() == 'channels_first':
+                if include_top:
+                    weights_path = get_file('resnext_8_16_th_dim_ordering_th_kernels.h5',
+                                            TH_WEIGHTS_PATH,
+                                            cache_subdir='models')
+                else:
+                    weights_path = get_file('resnext_8_16_th_dim_ordering_th_kernels_no_top.h5',
+                                            TH_WEIGHTS_PATH_NO_TOP,
+                                            cache_subdir='models')
+
+                model.load_weights(weights_path)
+
+                if K.backend() == 'tensorflow':
+                    warnings.warn('You are using the TensorFlow backend, yet you '
+                                  'are using the Theano '
+                                  'image dimension ordering convention '
+                                  '(`image_dim_ordering="th"`). '
+                                  'For best performance, set '
+                                  '`image_dim_ordering="tf"` in '
+                                  'your Keras config '
+                                  'at ~/.keras/keras.json.')
+                    convert_all_kernels_in_model(model)
+            else:
+                if include_top:
+                    weights_path = get_file('resnext_8_16_tf_dim_ordering_tf_kernels.h5',
+                                            TF_WEIGHTS_PATH,
+                                            cache_subdir='models')
+                else:
+                    weights_path = get_file('resnext_8_16_tf_dim_ordering_tf_kernels_no_top.h5',
+                                            TF_WEIGHTS_PATH_NO_TOP,
+                                            cache_subdir='models')
+
+                model.load_weights(weights_path)
+
+                if K.backend() == 'theano':
+                    convert_all_kernels_in_model(model)
+
+    return model
+
+
+def __initial_conv_block(input, weight_decay):
     channel_axis = 1 if K.image_data_format() == 'channels_first' else -1
 
+    x = Conv2D(64, (3, 3), padding='same', use_bias=False, kernel_initializer='he_normal',
+               kernel_regularizer=l2(weight_decay))(input)
     x = BatchNormalization(axis=channel_axis)(x)
     x = Activation('relu')(x)
+
+    return x
+
+
+def __initial_conv_block_inception(input, weight_decay):
+    channel_axis = 1 if K.image_data_format() == 'channels_first' else -1
+
+    x = Conv2D(64, (7, 7), padding='same', use_bias=False, kernel_initializer='he_normal',
+               kernel_regularizer=l2(weight_decay), strides=(2, 2))(input)
+    x = BatchNormalization(axis=channel_axis)(x)
+    x = Activation('relu')(x)
+
+    x = MaxPooling2D((3, 3), strides=(2, 2))(x)
+
     return x
 
 
@@ -227,7 +363,7 @@ def __create_res_next(nb_classes, img_input, include_top, depth=29, cardinality=
                Increasing cardinality improves classification accuracy,
         width: Width of the network.
         weight_decay: weight_decay (l2 norm)
-    Returns:a Keras Model
+    Returns: a Keras Model
     '''
 
     if type(depth) is list or type(depth) is tuple:
@@ -245,6 +381,61 @@ def __create_res_next(nb_classes, img_input, include_top, depth=29, cardinality=
         filters *= 2  # double the size of the filters
 
     x = __initial_conv_block(img_input, weight_decay)
+
+    # block 1 (no pooling)
+    for i in range(N[0]):
+        x = __bottleneck_block(x, filters_list[0], cardinality, width, strides=1, weight_decay=weight_decay)
+
+    N = N[1:]  # remove the first block from block definition list
+    filters_list = filters_list[1:]  # remove the first filter from the filter list
+
+    # block 2 to N
+    for block_idx, n_i in enumerate(N):
+        for i in range(n_i):
+            if i == 0:
+                x = __bottleneck_block(x, filters_list[block_idx], cardinality, width, strides=2,
+                                       weight_decay=weight_decay)
+            else:
+                x = __bottleneck_block(x, filters_list[block_idx], cardinality, width, strides=1,
+                                       weight_decay=weight_decay)
+
+    x = GlobalAveragePooling2D()(x)
+
+    if include_top:
+        x = Dense(nb_classes, use_bias=False, kernel_regularizer=l2(weight_decay),
+                  kernel_initializer='he_normal', activation='softmax')(x)
+
+    return x
+
+
+def __create_res_next_imagenet(nb_classes, img_input, include_top, depth, cardinality=32, width=4, weight_decay=5e-4):
+    ''' Creates a Wide Residual Network with specified parameters
+    Args:
+        nb_classes: Number of output classes
+        img_input: Input tensor or layer
+        include_top: Flag to include the last dense layer
+        depth: Depth of the network. List of integers.
+               Increasing cardinality improves classification accuracy,
+        width: Width of the network.
+        weight_decay: weight_decay (l2 norm)
+    Returns: a Keras Model
+    '''
+
+    if type(depth) is list or type(depth) is tuple:
+        # If a list is provided, defer to user how many blocks are present
+        N = list(depth)
+    else:
+        # Otherwise, default to 3 blocks each of default number of group convolution blocks
+        N = [(depth - 2) // 9 for _ in range(3)]
+
+    filters = 64
+    filters_list = []
+
+    for i in range(len(N)):
+        filters_list.append(filters)
+        filters *= 2  # double the size of the filters
+
+    x = __initial_conv_block_inception(img_input, weight_decay)
 
     # block 1 (no pooling)
     for i in range(N[0]):
